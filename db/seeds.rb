414198dd9db06e8d32addb9d841efc5c9baebc80
Sascha Gysel <sascha@gysel.biz>
2020-02-28 12:10:57 +0800

    Changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,25 @@
 require 'open-uri'
 require 'json'
-url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-response = open(url).read
-repo = JSON.parse(response)
-ingredients = repo["drinks"]
-ingredients.each do |ingredient|
-  new_ingredient = Ingredient.new(name: ingredient["strIngredient1"])
-  new_ingredient.save!
-end
-url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=m"
+
+("a".."b").each do |letter|
+url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}"
+puts "getting #{letter}"
 response = open(url).read
 cocktail_repo = JSON.parse(response)
 cocktails = cocktail_repo["drinks"]
-# p cocktails.first['strGlass']
+next if cocktails.nil?
 cocktails.each do |cocktail|
+  puts "Adding..."
+  puts cocktail['strDrink']
   new_cocktail = Cocktail.create(name: cocktail['strDrink'])
   i = 1
   loop do
     ingredient_name = cocktail["strIngredient#{i}"]
     ingredient_description = cocktail["strMeasure#{i}"]
     break if ingredient_name == nil
-    ingredient = Ingredient.find_by(name: ingredient_name)
-    if ingredient
-      Dose.create(cocktail: new_cocktail, ingredient: ingredient, description: ingredient_description)
-    else
-      new_ingredient = Ingredient.create(name: ingredient_name)
-      Dose.create(cocktail: new_cocktail, ingredient: new_ingredient, description: ingredient_description)
-    end
+    new_ingredient = Ingredient.find_or_create_by(name: ingredient_name)
+    Dose.create(cocktail: new_cocktail, ingredient: new_ingredient, description: ingredient_description)
     i += 1
   end
+end
 end
